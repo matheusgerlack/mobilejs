8ba31295176138184fbd56201ec0c4b785742ff7
Matheus Gerlack <matheusfgk@gmail.com>
2016-10-19 09:39:24 -0200

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,47 @@
+(function($) {
+
+ console.log('opa');
+
+//attach overlay to the page
+  //build overlay
+  $menuOverlay = $('<div class="menu-overlay"></div>');
+
+  //apend menu to overlay
+  $(".nav-mobile").appendTo($menuOverlay);
+  //add close button to overlay
+  $('#close-menu').appendTo($menuOverlay);
+
+  //append overlay to body
+  $("body").append($menuOverlay);
+
+//hide main navigation 
+//CSS
+
+//show mobile navigation when click the button
+$("#bars-menu").click(function(event){
+
+  event.preventDefault();
+  $menuOverlay.fadeIn('slow');
+  $('#bars-menu').fadeOut('slow'); 
+
+});
+
+//attach button to nav
+//CSS
+
+//format mobile links inside the page
+//pending
+
+//close navigation
+$("#close-menu").click(function(event){
+
+  event.preventDefault();
+  $menuOverlay.fadeOut('medium');
+  $('#bars-menu').fadeIn('medium'); 
+
+});
+
+//show mobile buttons in the page
+//CSS
+
+})(jQuery); // Fully reference jQuery after this point.
